913bd309fdb5474846817a12ea1b409906e02159
Jack Bandy <jackbandy@icloud.com>
2017-11-18 18:49:50 -0500

    Added cluster module

---
+++ cluster.py
@@ -0,0 +1,15 @@
+'''
+cluster.py
+Cluster VAE-encoded features into words
+'''
+
+
+import numpy as np
+
+
+def createClusters(features):
+    # how many clusters though
+
+
+def createNClusters(features, n_clusters):
+    #scikit learn
